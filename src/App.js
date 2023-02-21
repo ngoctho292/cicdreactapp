155adedb4f78432a4f356a56722d6bf4fbbe071b
nT-090301 <nguyenngoctho393@gmail.com>
2023-02-21 15:09:47 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h321>chay oke roi</h32131>
+        <h2>chay oke roi</h2>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
