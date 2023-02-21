fe25510d2477b77e6bae5d2bb081ed02ea5c52f8
nT-090301 <nguyenngoctho393@gmail.com>
2023-02-21 13:16:42 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React so easyyyy
         </a>
       </header>
     </div>
