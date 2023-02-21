fcb4b7246d7862a315882cd8f197a4c5fd485d8f
nT-090301 <nguyenngoctho393@gmail.com>
2023-02-21 14:10:41 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>React So Easy</h1>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
