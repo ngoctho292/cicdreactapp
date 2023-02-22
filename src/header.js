dc461ad142ffbcc1d092f0c16c2b6723174aeb7d
thonn-hcm <nguyenngoctho393@gmail.com>
2023-02-22 13:40:39 +0700

    add icon

--- src/header.js
+++ src/header.js
@@ -1,11 +1,18 @@
 import React from 'react'
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEnvelope, faTicket } from '@fortawesome/free-solid-svg-icons'
+import { faHeart, faSquare } from '@fortawesome/free-regular-svg-icons'
 const header = () => {
     return (
         <div className="header">
             <h2>
                 đây là header va iis
             </h2>
+            {/* <FontAwesomeIcon icon="fa-star" /> */}
+            <FontAwesomeIcon icon={faEnvelope} />
+            <FontAwesomeIcon icon={faTicket} />
+            <FontAwesomeIcon icon={faHeart} />
+            <FontAwesomeIcon icon={faSquare} />
         </div>
     )
 }
