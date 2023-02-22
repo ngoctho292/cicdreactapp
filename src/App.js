c28024b486054fda6b7dc10022a2f9fc9b316254
thonn-hcm <nguyenngoctho393@gmail.com>
2023-02-22 09:06:37 +0700

    commit cuoi cung

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import Header from './header';
+import Footer from './footer';
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
         >
           Learn React
         </a>
+        <Footer />
       </header>
     </div>
   );
---
+++ src/footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const footer = () => {
+    return (
+        <div className="footer">
+            <h2>
+                đây là footer
+            </h2>
+        </div>
+    )
+}
+
+export default footer
