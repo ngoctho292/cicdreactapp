d21fcec484c4a047ae5b47d9344c33d56fa181e2
thonn-hcm <nguyenngoctho393@gmail.com>
2023-02-22 08:27:45 +0700

    update appjs

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React so easyyy
+          Learn React
         </a>
       </header>
     </div>
